de012b6c3863674b6a6d32d65b72668d4baf5a31
Heber, Markus <heber@geomarketing.de>
2025-09-05 09:55:43 +0200

    test: added a test for symbols with transparent fill and nontransparent stroke

---
+++ data/styles/point_transparent_fill.ts
@@ -0,0 +1,22 @@
+import { Style } from 'geostyler-style';
+
+const pointSimple: Style = {
+  name: 'QGIS Style',
+  rules: [{
+    name: 'QGIS Simple Symbol',
+    symbolizers: [{
+      kind: 'Mark',
+      wellKnownName: 'circle',
+      color: '#FF3C3C',
+      fillOpacity: 0,
+      radius: 2,
+      opacity: 1,
+      rotate: 0,
+      strokeColor: '#232323',
+      strokeOpacity: 1,
+      strokeWidth: 0
+    }]
+  }]
+} as Style;
+
+export default pointSimple;
